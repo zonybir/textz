b6a38ce08268a962291de1a2ab0bbd3e3ddf3f4c
zonybir <zonybir@icloud.com>
2015-09-17 23:33:34 +0800

    弹出框

---
+++ js/z.js
@@ -0,0 +1,24 @@
+/*
+
+position:  left   top  right bottom center  {left:X,top:X}
+fixed:true  false 默认true
+t: 若有此属性  带标题 
+tColor:  带标题 此属性生效  标题颜色
+tfSize:标题字体大小
+z:弹框内容
+zColor:弹框字体颜色
+zfSize:弹框内容字体大小
+type: 1 带确认按钮 （重要性提示） 2 无确认按钮 空白点击隐藏（不重要提示性用）  3带确定与取消 返回true false(用户选择性用) 
+border:'1px solid #ccc' 弹出框的边框
+box-show:弹出框的阴影
+box-bgColor:弹出框背景色
+*/
+function(window){
+	function tk(obj){
+		if (typeof obj === 'string'){
+
+		}else{
+
+		}
+	}
+}(window);
