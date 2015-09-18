2da2daf13c255a7a044cc92e7d032f949cb18896
zonybir <zonybir@icloud.com>
2015-09-18 17:02:17 +0800

    弹出框html

--- js/z.js
+++ js/z.js
@@ -13,7 +13,8 @@ border:'1px solid #ccc' 弹出框的边框
 box-show:弹出框的阴影
 box-bgColor:弹出框背景色
 */
-function(window){
+(function(window){
+	var d=document;
 	function tk(obj){
 		if (typeof obj === 'string'){
 
@@ -21,4 +22,57 @@ function(window){
 
 		}
 	}
-}(window);
+	var cover=d.createElement('div');
+	var coverStyle={
+		position:'fixed',
+		width:'100%',
+		height:'100%',
+		zIndex:999,
+		backgroundColor:'rgba(0,0,0,.6)',
+		top:'0px'
+	};
+	function setStyle(ele,style){
+			for (var i in style){
+				ele.style[i]=style[i];
+			}
+	}
+	setStyle(cover,coverStyle);
+	
+	var box=d.createElement('div');
+	var boxStyle={
+			width:'200px',
+			backgroundColor:'#fff',
+			margin:'0 auto',
+			top:'10%',
+			position:'relative'
+	}
+	setStyle(box,boxStyle);
+	cover.appendChild(box);
+
+	var title=d.createElement('div');
+	var titleStyle={
+			fontSize:'18px',
+			color:'#CCC',
+			borderBottom:'1px solid #ccc',
+			backgroundColor:'#eee',
+			textAlign:'center',
+			padding:'5px 10px'
+	}
+	setStyle(title,titleStyle);
+	title.innerHTML='Title';
+	box.appendChild(title);
+
+	var z=d.createElement('div');
+	var zStyle={
+		fontSize:'16px',
+		textAlign:'left',
+		color:'#666',
+		backgroundColor:'#ccc',
+		padding:'10px 10px'
+	}
+	setStyle(z,zStyle);
+	z.innerHTML='z tanchu kuang';
+	box.appendChild(z);
+
+	d.querySelectorAll('body')[0].appendChild(cover);
+}(window));
