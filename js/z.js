6206f6d47ac00de107e9a4fd9770b3c5b467eb0b
zony-uyixing <zonybir@icloud.com>
2015-09-22 14:47:13 +0800

    整理代码结构001

--- js/z.js
+++ js/z.js
@@ -14,109 +14,104 @@ box-show:弹出框的阴影
 box-bgColor:弹出框背景色
 */
 (function(window){
-	var d=document;
-	function tk(obj){
-		if (typeof obj === 'string'){
-
-		}else{
-
-		}
-	}
-	var cover=d.createElement('div');
-	var coverStyle={
-		position:'fixed',
-		width:'100%',
-		height:'100%',
-		zIndex:999,
-		backgroundColor:'rgba(0,0,0,.6)',
-		top:'0px'
-	};
-	function setStyle(ele,style){
-			for (var i in style){
-				ele.style[i]=style[i];
-			}
-	}
-	setStyle(cover,coverStyle);
-	
-	var box=d.createElement('div');
-	var boxStyle={
+	var d=document,
+	cover=d.createElement('div'),
+	box=cover.cloneNode(true),
+	title=cover.cloneNode(true),
+	z=cover.cloneNode(true),
+	footer=cover.cloneNode(true),
+	btnSure=d.createElement('span'),
+	btnCancel=btnSure.cloneNode(true),
+	dialog={
+		coverStyle:{
+		 	position:'fixed',
+			width:'100%',
+			height:'100%',
+			zIndex:999,
+			backgroundColor:'rgba(0,0,0,.6)',
+			top:'0px'
+			},
+		boxStyle:{
 			width:'200px',
 			backgroundColor:'#fff',
 			margin:'0 auto',
 			top:'10%',
 			position:'relative'
-	}
-	setStyle(box,boxStyle);
-	cover.appendChild(box);
-
-	var title=d.createElement('div');
-	var titleStyle={
+			},
+		titleStyle:{
 			fontSize:'16px',
 			color:'#fff',
 			borderBottom:'1px solid #ccc',
 			backgroundColor:'#8593B7',
 			textAlign:'center',
 			padding:'5px 10px'
+			},
+		zStyle:{
+			fontSize:'14px',
+			textAlign:'left',
+			color:'#666',
+			backgroundColor:'#fff',
+			padding:'10px 15px',
+			maxHeight:'221px',
+			overflow:'auto'
+			},
+		footerStyle:{
+			padding:'5px 5px',
+			backgroundColor:'#D2D2D2',
+			color:'#333',
+			fontSize:'16px',
+			textAlign:'right'
+		},
+		btnCancelStyle:{
+			width:'40px',
+			padding:'2px 10px',
+			color:'#fff',
+			backgroundColor:'#8593B7',
+			textAlign:'center',
+			cursor:'pointer',
+			fontSize:'14px',
+			borderRadius:'5px',
+			marginRight:'10px'
+			},
+		btnSureStyle:{
+			width:'40px',
+			padding:'2px 10px',
+			color:'#fff',
+			backgroundColor:'#8593B7',
+			textAlign:'center',
+			cursor:'pointer',
+			fontSize:'14px',
+			borderRadius:'5px',
+			marginRight:'5px'
+			}
+	};
+
+	function setStyle(ele,style){
+			for (var i in style){
+				ele.style[i]=style[i];
+			}
 	}
-	setStyle(title,titleStyle);
+
+	
+	cover.appendChild(box);
+
+
+	
 	title.innerHTML='提示';
 	box.appendChild(title);
 
-	var z=d.createElement('div');
-	var zStyle={
-		fontSize:'14px',
-		textAlign:'left',
-		color:'#666',
-		backgroundColor:'#fff',
-		padding:'10px 15px',
-		maxHeight:'221px',
-		overflow:'auto'
-	};
-	setStyle(z,zStyle);
-	var str='我多想回到家乡，再回到她的身旁。让她的温柔善良，来抚慰我的心伤。'
+	
+	
+	var str='确认同意协议，继续下一步操作？';
 	/*for (var i=0;i<5;i++){
 		str+=str;
 	}*/
 	z.innerHTML=str;
 	box.appendChild(z);
 
-	var footer=d.createElement('div');
-	var footerStyle={
-		padding:'5px 5px',
-		backgroundColor:'#D2D2D2',
-		color:'#333',
-		fontSize:'16px',
-		//position:'relative'
-		textAlign:'right'
-	};
-	var btnSure=d.createElement('span');
-	var btnStyle={
-		//display:'block',
-		width:'40px',
-		padding:'2px 10px',
-		//position:'absolute',
-		color:'#fff',
-		backgroundColor:'#8593B7',
-		textAlign:'center',
-		cursor:'pointer',
-		fontSize:'14px',
-		borderRadius:'5px',
+	
 
-	};
-	var btnCancel=d.createElement('span');
-	var btnCancelStyle={
-		//'float':'right',
-		marginRight:'10px'
-
-	};
-	var btnSureStyle={
-		marginRight:'5px'
-	};
-	setStyle(footer,footerStyle);
-	setStyle(btnSure,btnStyle);
-	setStyle(btnSure,btnSureStyle);
-	setStyle(btnCancel,btnStyle);
-	setStyle(btnCancel,btnCancelStyle);
+	
 	
 	btnSure.innerHTML='确定';
 	btnCancel.innerHTML='取消';
@@ -177,21 +172,55 @@ box-bgColor:弹出框背景色
 		}
 	};
 	zDialog.prototype.initStyle=function(options){
-		if (hasPrototype(options.coverStyle)) setStyle(cover,options.coverStyle);
-		if (hasPrototype(options.boxStyle)) setStyle(box,options.boxStyle);
-		if (hasPrototype(options.titleStyle)) setStyle(title,options.titleStyle);
-		if (hasPrototype(options.zStyle)) setStyle(z,options.zStyle);
-		if (hasPrototype(options.footerStyle)) setStyle(footer,options.footerStyle);
+		if (hasPrototype(options.coverStyle)) for(var i in options.coverStyle) dialog.coverStyle[i]=options.coverStyle[i];
+		if (hasPrototype(options.boxStyle)) for(var i in options.boxStyle) dialog.boxStyle[i]=options.boxStyle[i];
+		if (hasPrototype(options.titleStyle)) for(var i in options.titleStyle) dialog.titleStyle[i]=options.titleStyle[i];
+		if (hasPrototype(options.zStyle)) for(var i in options.zStyle) dialog.zStyle[i]=options.zStyle[i];
+		if (hasPrototype(options.footerStyle)) for(var i in options.footerStyle) dialog.footerStyle[i]=options.footerStyle[i];
+		if(hasPrototype(options.btnSureStyle)) for(var i in options.btnSureStyle) dialog.btnSureStyle[i]=options.btnSureStyle[i];
+		if(hasPrototype(options.btnCancelStyle)) for(var i in options.btnCancelStyle) dialog.btnCancelStyle[i]=options.btnCancelStyle[i];
 	}
-	window.$z=zDialog;
+	
+	function overallStyle(){
+		setStyle(cover,dialog.coverStyle);
+		setStyle(box,dialog.boxStyle);
+		setStyle(title,dialog.titleStyle);
+		setStyle(z,dialog.zStyle);
+		setStyle(footer,dialog.footerStyle);
+		setStyle(btnSure,dialog.btnSureStyle);
+		setStyle(btnCancel,dialog.btnCancelStyle);
+		for (var i in dialog) {
+			i=i.substring(0,-6);
+			console.log(i);
+		}
+	}
+	window.$z=function(str,options){
+		return new zDialog(str,options);
+	};
+	$z.prototype.init=function(options){
+		zDialog.prototype.initStyle(options);
+	}
+	$z.init=$z.prototype.init;
+	$z.s=dialog;
 }(window));
 var a='312';
 //console.log($z('body',{width:123,color:'red'}));
 var av={x:1,b:{3:3}};
 console.log(typeof av);
+console.log($z)
+$z.init({
+	coverStyle:{
+		backgroundColor:'red'	
+	}
+})
 window.onload=function(){
 	$z('.text1',{
 		title:'警告！',
 		z:'请不要再觉得她狠可爱。'
-	})
+	});
+	var i=4,b=i;
+	b=5;
+	console.log(i);
+
+
 }
