96b2ee2f90cfc0fbc8f1b5f8db6098e82734a5eb
zony-uyixing <zonybir@icloud.com>
2015-09-28 17:26:49 +0800

    框架基本完成 缺 box footer样式设定 2015年09月28日17:26:44

--- js/z.js
+++ js/z.js
@@ -84,16 +84,14 @@ btnCancelText:取消按钮文字	         type取值  2  时生效
 		else if (target.attachEvent) target.attachEvent('on'+type,function(evet){return handler.call(target,event);})
 		else {z.innerHTML='浏览器版本过低，请升级你的浏览器方可正常使用。谢谢！';}
 	}
-	function close(){cover.parentNode.removeChild(cover);removeEvent();}
 	function stop(event){
 		var e=event || window.event;
 		if (e.stopPropagation) e.stopPropagation();
 		else if(window.event) window.event.cancelBubble = true;
 	}
-
 	function show(options){
 		this.options=options;
-		this.show();		 
+		this.init();		 
 	};
 	show.prototype={
 		cloneAndApeendNode:function(){
@@ -106,6 +104,7 @@ btnCancelText:取消按钮文字	         type取值  2  时生效
 			this.box.appendChild(this.title);
 			this.box.appendChild(this.z);
 			this.box.appendChild(this.footer);
+			this.cover=cover.cloneNode(true);
 		},
 		setT:function(){
 			var _t=this;
@@ -126,35 +125,51 @@ btnCancelText:取消按钮文字	         type取值  2  时生效
 			}
 		},
 		setBtn:function(){
-			if (typeof this.options.type !== 'underfind'){
-				switch (this.options.type){
+			var _t=this;
+			if (typeof _t.options.type !== 'underfind'){
+				switch (_t.options.type){
 					case 0:{
-						this.footer.parentNode.removeChild(this.footer);
+						_t.footer.parentNode.removeChild(_t.footer);
+						addEvent(_t.cover,'click',function(){_t.close();});
 						break;
 					}
 					case 1:{
-						this.footer.appendChild(this.btnSure);
-						if(this.options.btnSureText) this.btnSure.innerHTML=this.options.btnSureText;
-						else this.btnSure.innerHTML='确定';
+						_t.footer.appendChild(_t.btnSure);
+						if(_t.options.btnSureText) _t.btnSure.innerHTML=_t.options.btnSureText;
+						else _t.btnSure.innerHTML='确定';
+						if(_t.checkCallback('sureFun')) addEvent(_t.btnSure,'click',function(){_t.sureFun();});
+						addEvent(_t.btnSure,'click',function(){_t.close();});
+						_t.clickCoverHide();
 						break;
 					}
 					case 2:{
-						this.footer.appendChild(this.btnCancel);
-						if(this.options.btnCancelText) this.btnCancel.innerHTML=this.options.btnCancelText;
-						else this.btnCancel.innerHTML='取消';
-						this.footer.appendChild(this.btnSure);
-						if(this.options.btnSureText) this.btnSure.innerHTML=this.options.btnSureText;
-						else this.btnSure.innerHTML='确定';
+						_t.footer.appendChild(_t.btnCancel);
+						if(_t.options.btnCancelText) _t.btnCancel.innerHTML=_t.options.btnCancelText;
+						else _t.btnCancel.innerHTML='取消';
+						_t.footer.appendChild(_t.btnSure);
+						if(_t.options.btnSureText) _t.btnSure.innerHTML=_t.options.btnSureText;
+						else _t.btnSure.innerHTML='确定';
+						addEvent(_t.btnSure,'click',function(){_t.close();});
+						if(_t.checkCallback('sureFun')) addEvent(_t.btnSure,'click',function(){_t.sureFun();});
+						addEvent(_t.btnCancel,'click',function(){_t.close();});
+						if(_t.checkCallback('cancelFun')) addEvent(_t.btnCancel,'click',function(){_t.cancelFun();});
 						break;
 					}
 					default:
 						break;
 				}
-			}else {}
+			}else {	//无type属性时 的默认类型
+				if(_t.options.btnSureText) _t.btnSure.innerHTML=_t.options.btnSureText;
+				else _t.btnSure.innerHTML='确定';
+				_t.footer.appendChild(_t.btnSure);
+				if(_t.checkCallback('sureFun')) addEvent(_t.btnSure,'click',function(){_t.sureFun();});
+				addEvent(_t.btnSure,'click',function(){_t.close();});
+				_t.clickCoverHide();
+			}
 		},
 		overallStyle:function(){
 			var _t=this;
-			_t.setStyle(cover,dialog.coverStyle);
+			_t.setStyle(this.cover,dialog.coverStyle);
 			_t.setStyle(this.box,dialog.boxStyle);
 			_t.setStyle(this.title,dialog.titleStyle);
 			_t.setStyle(this.z,dialog.zStyle);
@@ -162,16 +177,36 @@ btnCancelText:取消按钮文字	         type取值  2  时生效
 			_t.setStyle(this.btnSure,dialog.btnSureStyle);
 			_t.setStyle(this.btnCancel,dialog.btnCancelStyle);
 		},
+		clickCoverHide:function(){
+			var _t=this;
+			if (this.options.clickCoverHide == true || this.options.clickCoverHide == 'true') addEvent(this.cover,'click',function(){_t.close();})
+		},
+		checkCallback:function(str){
+			if(typeof this.options[str] === 'function') return true;
+			else return false;
+		},
+		sureFun:function(){
+			this.options.sureFun();
+		},
+		cancelFun:function(){
+			this.options.cancelFun();
+		},
+		close:function(){
+			this.cover.parentNode.removeChild(this.cover);
+		},
 		setStyle:function(ele,obj){for(var i in obj)ele.style[i]=obj[i];},
-		show:function(){
+		init:function(){
 			this.cloneAndApeendNode();
 			this.overallStyle();
 			this.setT();
 			this.setZ();
 			this.setBtn();
+			var _t=this;
+
 			
-			cover.appendChild(this.box);
-			d.querySelectorAll('body')[0].appendChild(cover);
+			addEvent(this.box,'click',stop);
+			this.cover.appendChild(this.box);
+			d.querySelectorAll('body')[0].appendChild(this.cover);
 		}
 	}	
 	function zDialog(str,options){zDialog.prototype.start(str,options);};
@@ -196,21 +231,10 @@ btnCancelText:取消按钮文字	         type取值  2  时生效
 	};
 	zDialog.init=zDialog.prototype.initStyle;
 	zDialog.start=zDialog.prototype.start;
-	function addEventList(){
-		addEvent(btnSure,'click',function(){close();});
-		addEvent(btnCancel,'click',function(){close();});
-		addEvent(cover,'click',function(){close();})
-		addEvent(box,'click',function(e){stop(e);});
-		document.removeEventListener('click',cover,false);
-	}
-	function removeEvent(){
-		document.removeEventListener('click',clickhandler,false);
-		document.removeEventListener('click',btnCancel,false);		
-		document.removeEventListener('click',box,false);
-	}
 	window.$z=zDialog;
 }(window));
 window.onload=function(){
+	var a=321;
 	$z.init({
 		boxStyle:{
 			backgroundColor:'red',
@@ -227,7 +251,29 @@ window.onload=function(){
 		zStyle:{
 			textAlign:'center',
 			color:'red'
-		}
+		},
+		sureFun:function(){
+			console.log('zonybir\'s love.');
+		},
+		t:true,
+		clickCoverHide:true
 	});
+	$z('.text2',{
+		type:2,
+		z:'我们都还在这里?',
+		tText:'提示2',
+		tStyle:{
+			textAlign:'center'
+		},
+		sureFun:function(){
+			console.log('你选择的是的，对不起，回答错误。');
+			a++;
+		},
+		cancelFun:function(){
+			console.log('你选择的不是，恭喜你答对了。');
+		},
+		btnSureText:'是的',
+		btnCancelText:'不是'
+	})
 	//$z('.text2',{z:'fefefefefwefwfwfewfwefwefwfe'});
 }
